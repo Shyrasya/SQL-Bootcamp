14e290fe66cdc79c5b64f093fd44efc351de2988
Alexandra <41370183+Shyrasya@users.noreply.github.com>
2024-10-28 19:40:12 +0300

    Delete SQL_beginner.Team_00 directory

--- SQL_beginner.Team_00/src/ex01/team00_ex01.sql
+++
@@ -1,8 +0,0 @@
-CREATE VIEW ex01 AS (
-    SELECT *
-    FROM done_tour
-    WHERE total_cost = (SELECT MAX(total_cost) FROM done_tour)
-    UNION
-    SELECT *
-    FROM ex00
-    ORDER BY total_cost, tour);
